dd486508eb4762fc156dcaa873009c68e828a6ef
doris <doris.anic@stud.fh-campuswien.ac.at>
2025-03-11 10:05:20 +0100

    framework implemented (vue)

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function showMessage() {
-    alert("Hello from JavaScript!");
-    console.log("Button clicked!");
-}
+// Create Vue application instance
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: "Hello from Vue!",
+        };
+    },
+    methods: {
+        showMessage() {
+            // Show message via alert
+            alert("Hello from Vue!");
+
+            // Log message to the console
+            console.log("Button clicked!");
+
+            // You can also change the message after button click
+            this.message = "You clicked the button!";
+        }
+    }
+}).mount('#app');
